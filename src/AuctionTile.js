9463f1e4fc0f95827ddd03312560e5213fe16738
ndebiec <nadia.debiec@netguru.pl>
2020-07-15 15:52:04 +0200

    Export styles for AuctionTile to CSS Module

--- src/AuctionTile.js
+++ src/AuctionTile.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import styles from './AuctionTile.module.css';
 
 const AuctionTile = (props) => {
   return (
-    <div className="AuctionTile">
+    <div className={styles.AuctionTile}>
       <h3>{props.name}</h3>
       <p>Only {props.price} $</p>
     </div>
   )
 }
-
 
 export default AuctionTile;
